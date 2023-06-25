7573b55ed22267e5c3a3063b8bff78db2cb0ffa2
Mehul <mehkumawat@gmail.com>
2023-06-25 23:50:14 +0530

    ep5

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,14 @@ import RestaurantCard from './RestaurantCard';
 import resList from '../utils/sampleData';
 
 
-const Body = () => {
+export const Body = () => {
     return (
       <div className="body">
-        <div className="search">search</div>
+        <div className="Filter">
+          <button className='filter-btn'>
+            Top Rated restaurant
+          </button>
+        </div>
         <div className="res-container">
           {resList.map((restaurant) => (<RestaurantCard key={restaurant.data.id} resData={restaurant}/>))}
         </div>
